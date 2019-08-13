d4e8dc56d741d169666bd3abfc6f9a7fc6bae1b6
boruch d <dubywider@gmail.com>
2019-08-13 17:08:14 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,12 +1,19 @@
 def unsafe?(speed)
+  if speed > 60||40 > speed
+    return true
+  else 
+    return false
 
 end
-
+end
 
 
 def not_safe?(speed)
-	
+	  speed > 60||40 > speed ?  true :  false
+	 
+    
+   
 end
-	
+
 
 
